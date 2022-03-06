b1a2315efc8b96b126f3ac009d649c57f02d6ead
mothkury-vishnuvardhan <mvishnuvardhanchary@gmail.com>
2022-03-07 00:13:50 +0530

    assessment

--- FileHandlingproject/src/com/project/assistes3/FileHandling1.java
+++ FileHandlingproject/src/com/project/assistes3/FileHandling1.java
@@ -1,7 +1,8 @@
 package com.project.assistes3;
 import java.io.*; 
 public class FileHandling1{ 
- public static void main(String[] args){         //Reading an Existing file        
+ public static void main(String[] args){    
+	 //Reading an Existing file        
 	 try   { 
 	        
 	 FileReader r = new FileReader("C:\\Users\\Vishnu Vardhan\\OneDrive\\Desktop\\project");          
--- FileHandlingproject/src/com/project/assistes3/FileHandling3.java
+++ FileHandlingproject/src/com/project/assistes3/FileHandling3.java
@@ -1,7 +1,8 @@
 package com.project.assistes3;
 import java.io.*;  
 public class FileHandling3 { 
- public static void main(String[] args){          // Append text to Existing File. 
+ public static void main(String[] args){  
+	 // Append text to Existing File. 
 	 
  String path = "C:\\Users\\Vishnu Vardhan\\OneDrive\\Desktop\\project\\demo.txt"; 
   String text = "  Currently doing training at Simplilearn";       
